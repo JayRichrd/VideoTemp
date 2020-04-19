3162d3ebe21888ba89409667089eff51fa77ee59
cainjiang <cainjiang@tencent.com>
2020-04-19 23:33:35 +0800

    feat(check): 成功导入lame库，并检验输出版本号

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,8 +1,9 @@
 #include <jni.h>
 #include <string>
+#include "lame/lame.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_cain_videotemp_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
     std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF(get_lame_version());
 }
