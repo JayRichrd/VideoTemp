abf85f5904a3df4ee665575da2eaa156c6aca448
cainjiang <cainjiang@tencent.com>
2020-05-11 00:46:58 +0800

    feat(OpenGL): 添加openGL3.0示例 显示一张图片

--- app/src/main/java/com/cain/videotemp/pic/opengl/BitmapDrawer.kt
+++ app/src/main/java/com/cain/videotemp/pic/opengl/BitmapDrawer.kt
@@ -1,7 +1,7 @@
 package com.cain.videotemp.pic.opengl
 
 import android.graphics.Bitmap
-import android.opengl.GLES20
+import android.opengl.GLES30
 import android.opengl.GLUtils
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
@@ -58,56 +58,56 @@ class BitmapDrawer(private val mBitmap: Bitmap) {
 
     private fun createGLPrg() {
         if (mProgram == -1) {
-            val vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, getVertexShader())
-            val fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, getFragmentShader())
+            val vertexShader = loadShader(GLES30.GL_VERTEX_SHADER, getVertexShader())
+            val fragmentShader = loadShader(GLES30.GL_FRAGMENT_SHADER, getFragmentShader())
             //创建OpenGL ES程序，注意：需要在OpenGL渲染线程中创建，否则无法渲染
-            mProgram = GLES20.glCreateProgram()
+            mProgram = GLES30.glCreateProgram()
             //创建OpenGL ES程序，注意：需要在OpenGL渲染线程中创建，否则无法渲染
-            mProgram = GLES20.glCreateProgram()
+            mProgram = GLES30.glCreateProgram()
             //将顶点着色器加入到程序
-            GLES20.glAttachShader(mProgram, vertexShader)
+            GLES30.glAttachShader(mProgram, vertexShader)
             //将片元着色器加入到程序中
-            GLES20.glAttachShader(mProgram, fragmentShader)
+            GLES30.glAttachShader(mProgram, fragmentShader)
             //连接到着色器程序
-            GLES20.glLinkProgram(mProgram)
-            mVertexPosHandler = GLES20.glGetAttribLocation(mProgram, "aPosition")
-            mTexturePosHandler = GLES20.glGetAttribLocation(mProgram, "aCoordinate")
-            mTextureHandler = GLES20.glGetUniformLocation(mProgram, "uTexture")
+            GLES30.glLinkProgram(mProgram)
+            mVertexPosHandler = GLES30.glGetAttribLocation(mProgram, "aPosition")
+            mTexturePosHandler = GLES30.glGetAttribLocation(mProgram, "aCoordinate")
+            mTextureHandler = GLES30.glGetUniformLocation(mProgram, "uTexture")
         }
         //使用OpenGL程序
-        GLES20.glUseProgram(mProgram)
+        GLES30.glUseProgram(mProgram)
     }
 
     private fun activateTexture() {
         //激活指定纹理单元
-        GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
+        GLES30.glActiveTexture(GLES30.GL_TEXTURE0)
         //绑定纹理ID到纹理单元
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureId)
+        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, mTextureId)
         //将激活的纹理单元传递到着色器里面
-        GLES20.glUniform1i(mTextureHandler, 0)
+        GLES30.glUniform1i(mTextureHandler, 0)
         //配置边缘过渡参数
-        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR.toFloat())
-        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR.toFloat())
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE)
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE)
+        GLES30.glTexParameterf(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_MIN_FILTER, GLES30.GL_LINEAR.toFloat())
+        GLES30.glTexParameterf(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_MAG_FILTER, GLES30.GL_LINEAR.toFloat())
+        GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_WRAP_S, GLES30.GL_CLAMP_TO_EDGE)
+        GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_WRAP_T, GLES30.GL_CLAMP_TO_EDGE)
     }
 
     private fun bindBitmapToTexture() {
         if (!mBitmap.isRecycled) {
             //绑定图片到被激活的纹理单元
-            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, mBitmap, 0)
+            GLUtils.texImage2D(GLES30.GL_TEXTURE_2D, 0, mBitmap, 0)
         }
     }
 
     private fun doDraw() {
         //启用顶点的句柄
-        GLES20.glEnableVertexAttribArray(mVertexPosHandler)
-        GLES20.glEnableVertexAttribArray(mTexturePosHandler)
+        GLES30.glEnableVertexAttribArray(mVertexPosHandler)
+        GLES30.glEnableVertexAttribArray(mTexturePosHandler)
         //设置着色器参数， 第二个参数表示一个顶点包含的数据数量，这里为xy，所以为2
-        GLES20.glVertexAttribPointer(mVertexPosHandler, 2, GLES20.GL_FLOAT, false, 0, mVertexBuffer)
-        GLES20.glVertexAttribPointer(mTexturePosHandler, 2, GLES20.GL_FLOAT, false, 0, mTextureBuffer)
+        GLES30.glVertexAttribPointer(mVertexPosHandler, 2, GLES30.GL_FLOAT, false, 0, mVertexBuffer)
+        GLES30.glVertexAttribPointer(mTexturePosHandler, 2, GLES30.GL_FLOAT, false, 0, mTextureBuffer)
         //开始绘制
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLE_STRIP, 0, 4)
     }
 
     private fun getVertexShader(): String {
@@ -132,10 +132,10 @@ class BitmapDrawer(private val mBitmap: Bitmap) {
 
     private fun loadShader(type: Int, shaderCode: String): Int {
         //根据type创建顶点着色器或者片元着色器
-        val shader = GLES20.glCreateShader(type)
+        val shader = GLES30.glCreateShader(type)
         //将资源加入到着色器中，并编译
-        GLES20.glShaderSource(shader, shaderCode)
-        GLES20.glCompileShader(shader)
+        GLES30.glShaderSource(shader, shaderCode)
+        GLES30.glCompileShader(shader)
         return shader
     }
 
@@ -157,10 +157,10 @@ class BitmapDrawer(private val mBitmap: Bitmap) {
     }
 
     fun release() {
-        GLES20.glDisableVertexAttribArray(mVertexPosHandler)
-        GLES20.glDisableVertexAttribArray(mTexturePosHandler)
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0)
-        GLES20.glDeleteTextures(1, intArrayOf(mTextureId), 0)
-        GLES20.glDeleteProgram(mProgram)
+        GLES30.glDisableVertexAttribArray(mVertexPosHandler)
+        GLES30.glDisableVertexAttribArray(mTexturePosHandler)
+        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, 0)
+        GLES30.glDeleteTextures(1, intArrayOf(mTextureId), 0)
+        GLES30.glDeleteProgram(mProgram)
     }
 }
--- app/src/main/java/com/cain/videotemp/pic/opengl/SimpleRender.kt
+++ app/src/main/java/com/cain/videotemp/pic/opengl/SimpleRender.kt
@@ -1,6 +1,6 @@
 package com.cain.videotemp.pic.opengl
 
-import android.opengl.GLES20
+import android.opengl.GLES30
 import android.opengl.GLSurfaceView
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
@@ -13,28 +13,28 @@ import javax.microedition.khronos.opengles.GL10
 class SimpleRender(private val drawers: MutableList<BitmapDrawer> = mutableListOf()) : GLSurfaceView.Renderer {
 
     override fun onDrawFrame(gl: GL10?) {
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT or GLES30.GL_DEPTH_BUFFER_BIT)
         drawers.forEach {
             it.draw()
         }
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
-        GLES20.glViewport(0, 0, width, height)
+        GLES30.glViewport(0, 0, width, height)
     }
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-        GLES20.glClearColor(0f, 0f, 0f, 0f)
+        GLES30.glClearColor(0f, 0f, 0f, 0f)
         //开启混合，即半透明
-        GLES20.glEnable(GLES20.GL_BLEND)
-        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA)
+        GLES30.glEnable(GLES30.GL_BLEND)
+        GLES30.glBlendFunc(GLES30.GL_SRC_ALPHA, GLES30.GL_ONE_MINUS_SRC_ALPHA)
         val textureIds = OpenGLTools.createTextureIds(drawers.size)
         for ((index, drawer) in drawers.withIndex()) {
             drawer.setTextureID(textureIds[index])
         }
     }
 
-    fun addDrawer(drawer: BitmapDrawer): Unit {
+    fun addDrawer(drawer: BitmapDrawer) {
         drawers.add(drawer)
     }
 }
