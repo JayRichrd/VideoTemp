1e07d2d98e3bb67b0dec2953392e17a64cd7f437
cainjiang <cainjiang@tencent.com>
2020-09-18 21:12:20 +0800

    feat(sox): add sox sh
    添加mac交叉编译sox静态库的sh脚本

---
+++ app/shell/sox/build_android.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# SoX Regression Test script: Lossless file conversion
+
+#NDK路径，这里需要替换成你自己的NDK路径
+NDK_BASE=/Users/cainjiang/SDK/android-ndk-r21b
+API=28
+NDK_SYSROOT=$NDK_BASE/platforms/android-28/arch-arm
+NDK_TOOLCHAIN_BASE=$NDK_BASE/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64
+CC=$NDK_TOOLCHAIN_BASE/bin/armv7a-linux-android$API-clang
+# CXX =$NDK_TOOLCHAIN_BASE/bin/armv7a-linux-android$API-clang++
+LD=$NDK_TOOLCHAIN_BASE/bin/armv7a-linux-androideabi-ld
+#编译产物输出路径
+PREFIX=/Users/cainjiang/WorkSet/sox/android_lib
+CWD=`pwd`
+PROJECT_ROOT=$CWD
+./configure \
+--prefix=$(pwd)/android_lib \
+--target=armv7a \
+--host=arm-linux-androideabi \
+--with-sysroot=$NDK_SYSROOT \
+--enable-static \
+--disable-shared \
+--disable-openmp \
+--without-libltdl
+CFLAGS='-O2' \
+CC=$CC \
+LD=$LD \
